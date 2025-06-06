8ce942a27795f25b14b6092f0fcc25705d1353ad
Florian Richter <flori@hey.com>
2025-06-06 12:26:46 +0200

    Fix drag and drop functionality and add + on hover (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createPayload } from "./obx.ts";
 import { fetch } from "@tauri-apps/plugin-http";
 import { getVersion } from "@tauri-apps/api/app";
+import { readTextFile } from "@tauri-apps/plugin-fs";
+import { getCurrentWebview } from "@tauri-apps/api/webview";
 
 getVersion().then((appVersion) => {
   document.querySelector("#version")!.textContent = appVersion;
@@ -9,6 +11,7 @@ getVersion().then((appVersion) => {
 const dropZone: HTMLDivElement | null = document.querySelector("#dropZone");
 const dropText: HTMLParagraphElement | null =
   document.querySelector("#dropText");
+const originalDropText = dropText?.textContent || "Datei auswählen";
 const preview: HTMLPreElement | null = document.querySelector("#preview");
 const apiKeyInput: HTMLInputElement | null = document.querySelector("#apiKey");
 const fileUpload: HTMLInputElement | null = document.querySelector("#obx");
@@ -32,14 +35,14 @@ if (apiKeyInput) {
 fileUpload?.addEventListener("change", () => {
   const file = fileUpload.files?.[0];
   if (file) {
-    handleFileUpload(file);
+    handleFileUpload(file.name, file.text());
   }
 });
 
 aufschlagInput?.addEventListener("change", () => {
   const file = fileUpload?.files?.[0];
   if (file) {
-    handleFileUpload(file);
+    handleFileUpload(file.name, file.text());
   }
 });
 
@@ -47,19 +50,25 @@ dropZone?.addEventListener("click", () => fileUpload?.click());
 
 dropZone?.addEventListener("drop", (ev) => {
   ev.preventDefault();
-
-  if (ev.dataTransfer?.items) {
-    const item = ev.dataTransfer?.items[0];
-    if (item.kind === "file") {
-      handleFileUpload(item.getAsFile()!);
-    }
-  } else if (ev.dataTransfer?.files) {
-    handleFileUpload(ev.dataTransfer?.files[0]);
-  }
 });
 
 dropZone?.addEventListener("dragover", (ev) => {
   ev.preventDefault();
+});
+
+getCurrentWebview().onDragDropEvent((ev) => {
+  if (ev.payload.type === "over" && dropText) {
+    dropText.textContent = "+";
+  } else if (ev.payload.type === "drop") {
+    handleFileUpload(
+      ev.payload.paths[0]?.split("/").pop() || "Dateiname unbekannt",
+      readTextFile(ev.payload.paths[0])
+    );
+  } else {
+    if (dropText) {
+      dropText.textContent = originalDropText;
+    }
+  }
 });
 
 document.querySelector("#showPreview")?.addEventListener("click", (ev) => {
@@ -92,15 +101,18 @@ function submit(payload: string) {
   };
 }
 
-async function handleFileUpload(file: File) {
+async function handleFileUpload(
+  name: string,
+  content: Promise<string> | string
+) {
   const mult = 1 + parseFloat(aufschlagInput?.value ?? "0") / 100;
   const parser = new DOMParser();
-  const parsed = parser.parseFromString(await file.text(), "application/xml");
+  const parsed = parser.parseFromString(await content, "application/xml");
 
   shortPayload = createPayload(parsed, mult, false);
   longPayload = createPayload(parsed, mult, true);
 
   if (dropText) {
-    dropText.textContent = file.name;
+    dropText.textContent = name;
   }
 }
